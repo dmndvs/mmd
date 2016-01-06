4fc2573e2b6ab68aba39608b9cc4357337ad5abd
dmndvs <domin.dvs@gmail.com>
2016-01-06 12:23:49 +0200

    added plugin ctools_content_type

---
+++ mmd_display_item.tpl.php
@@ -0,0 +1,10 @@
+<div class="clearfix <?php print $row;?>">
+  <div class="mmd-img"><a href="<?php print $src_path . $img;?>"><img src="<?php print $thumbnail_path . $img;?>"></a></div>
+  <div class="mmd-content">
+    <div class="mmd-title clearfix"><?php if($importance == '1'):?>
+      <img src="<?php print $thumbnail_path . $imp_img;?>">
+      <?php endif; print '<h2>'.$title.'</h2>';?></div>
+    <div class="mmd-created"><?php print $created;?></div>
+    <div class="mmd-data"><?php print $data;?></div>
+  </div>
+</div>
